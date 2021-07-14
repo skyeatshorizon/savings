a056848cc109c634717a27db4178d70f9227f6d4
Fabian Laurenz Fuhrmann <fuhrmann@dc-solutions.at>
2021-07-14 13:51:31 +0200

    fix compiler issues

--- eth/contracts/Saving.sol
+++ eth/contracts/Saving.sol
@@ -7,19 +7,19 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Saving is AccessControl {
-	address payable immutable rescueAddr;
-	uint256 immutable lockedUntil;
+	address public immutable beneficiary;
+	uint256 public immutable lockedUntil;
 	mapping(address => bool) used;
-	Counter rescueCnt;
+	Counters.Counter rescueCnt;
 
-	constructor(address _rescueAddr, uint256 _lockedUntil) public {
-		rescueAddr = _rescueAddr;
+	constructor(address _beneficiary, uint256 _lockedUntil) {
+		beneficiary = _beneficiary;
 		lockedUntil = _lockedUntil;
 	}
 
 	receive() external payable{
 		if(block.number >= lockedUntil){
-			rescueAddr.transfer(address(this).balance);
+			payable(beneficiary).transfer(address(this).balance);
 		}
 
 		if(hasRole(DEFAULT_ADMIN_ROLE, msg.sender)){
@@ -28,7 +28,7 @@ contract Saving is AccessControl {
 
 				uint minConfirmations = SafeMath.div(getRoleMemberCount(DEFAULT_ADMIN_ROLE), 2);
 				if(Counters.current(rescueCnt) > minConfirmations){
-					rescueAddr.transfer(address(this).balance);
+					payable(beneficiary).transfer(address(this).balance);
 				}
 
 				used[msg.sender] = true;
@@ -37,11 +37,11 @@ contract Saving is AccessControl {
 		}
 	}
 
-	function withdrawalERC20(address _erc20Addr){
+	function withdrawalERC20(address _erc20Addr) external {
 		uint minConfirmations = SafeMath.div(getRoleMemberCount(DEFAULT_ADMIN_ROLE), 2);
 		require((block.number >= lockedUntil) || (Counters.current(rescueCnt) > minConfirmations));
 		IERC20 token = IERC20(_erc20Addr);
 		uint256 balance = token.balanceOf(address(this));
-		token.transfer(rescueAddr, balance);
+		token.transfer(beneficiary, balance);
 	}
 }
