18d0f0467354f65910ad45e7568d8e2a102a77de
Fabian Laurenz Fuhrmann <fuhrmann@dc-solutions.at>
2021-07-20 12:18:50 +0200

    update contract

--- eth/contracts/Saving.sol
+++ eth/contracts/Saving.sol
@@ -5,43 +5,38 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Saving is AccessControl {
-	address public immutable beneficiary;
+contract Saving is AccessControl, Ownable {
 	uint256 public immutable lockedUntil;
-	mapping(address => bool) used;
-	Counters.Counter rescueCnt;
+	mapping(address => bool) private used;
+	Counters.Counter private rescueCnt;
 
-	constructor(address _beneficiary, uint256 _lockedUntil) {
-		beneficiary = _beneficiary;
+	constructor(uint256 _lockedUntil) {
 		lockedUntil = _lockedUntil;
+
+		_setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
 	}
 
-	receive() external payable{
-		if(block.number >= lockedUntil){
-			payable(beneficiary).transfer(address(this).balance);
+	receive() external payable {
+		uint minConfirmations = SafeMath.div(getRoleMemberCount(DEFAULT_ADMIN_ROLE), 2);
+
+		if(hasRole(DEFAULT_ADMIN_ROLE, msg.sender) && (msg.value == 505 wei)){ //SOS
+			require(!used[msg.sender], "Saving: cannot 505 twice");
+			used[msg.sender] = true;
+			Counters.increment(rescueCnt);
 		}
 
-		if(hasRole(DEFAULT_ADMIN_ROLE, msg.sender)){
-			if(msg.value == 505 wei) { //SOS
-				require(!used[msg.sender]);
-
-				uint minConfirmations = SafeMath.div(getRoleMemberCount(DEFAULT_ADMIN_ROLE), 2);
-				if(Counters.current(rescueCnt) > minConfirmations){
-					payable(beneficiary).transfer(address(this).balance);
-				}
-
-				used[msg.sender] = true;
-				Counters.increment(rescueCnt);
-			}
+		if((block.number > lockedUntil) || (Counters.current(rescueCnt) > minConfirmations)){
+			payable(owner()).transfer(address(this).balance);
 		}
 	}
 
 	function withdrawalERC20(address _erc20Addr) external {
 		uint minConfirmations = SafeMath.div(getRoleMemberCount(DEFAULT_ADMIN_ROLE), 2);
-		require((block.number >= lockedUntil) || (Counters.current(rescueCnt) > minConfirmations));
+		require((block.number > lockedUntil) || (Counters.current(rescueCnt) > minConfirmations), "Saving: cannot free funds yet");
 		IERC20 token = IERC20(_erc20Addr);
 		uint256 balance = token.balanceOf(address(this));
-		token.transfer(beneficiary, balance);
+		token.transfer(owner(), balance);
 	}
 }
