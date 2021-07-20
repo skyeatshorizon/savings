57d5e5b07a2605c6e217a5cb28df7a5d57913e1d
Fabian Laurenz Fuhrmann <fuhrmann@dc-solutions.at>
2021-07-20 14:50:32 +0200

    updatate contract, add ERC777 support

--- eth/contracts/Saving.sol
+++ eth/contracts/Saving.sol
@@ -6,13 +6,18 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC777/IERC777Recipient.sol";
+import "@openzeppelin/contracts/introspection/IERC1820Registry.sol";
 
-contract Saving is AccessControl, Ownable {
+contract Saving is AccessControl, Ownable, IERC777Recipient {
+	bytes32 constant private _TOKENS_RECIPIENT_INTERFACE_HASH = 0xb281fc8c12954d22544db45de3159a39272895b169a852b314f9cc762e44c53b;
+    IERC1820Registry constant internal _ERC1820_REGISTRY = IERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
 	uint256 public immutable lockedUntil;
 	mapping(address => bool) private used;
 	Counters.Counter private rescueCnt;
 
 	constructor(uint256 _lockedUntil) {
+		_ERC1820_REGISTRY.setInterfaceImplementer(address(0), _TOKENS_RECIPIENT_INTERFACE_HASH, address(this));
 		lockedUntil = _lockedUntil;
 
 		_setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
@@ -39,4 +44,15 @@ contract Saving is AccessControl, Ownable {
 		uint256 balance = token.balanceOf(address(this));
 		token.transfer(owner(), balance);
 	}
+
+	function tokensReceived(
+        address /*operator*/,
+        address /*from*/,
+        address /*to*/,
+        uint256 /*amount*/,
+        bytes calldata /*userData*/,
+        bytes calldata /*operatorData*/
+    ) external override {
+
+    }
 }
