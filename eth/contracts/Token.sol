69746a94c21da5ae7ab83dca4736c0c25134c846
Fabian Laurenz Fuhrmann <fuhrmann@dc-solutions.at>
2021-07-20 12:19:37 +0200

    add Token Mock

---
+++ eth/contracts/Token.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "/home/fabian/Dokumente/savings/node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+	constructor(uint256 _amount, string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+		_mint(msg.sender, _amount);
+	}
+}
