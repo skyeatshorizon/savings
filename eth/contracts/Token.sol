869c12b66ace628329eb0851d0acd09296130a58
Fabian Laurenz Fuhrmann <fuhrmann@dc-solutions.at>
2021-08-15 15:35:24 +0200

    fix import

--- eth/contracts/Token.sol
+++ eth/contracts/Token.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "/home/fabian/Dokumente/savings/node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
 	constructor(uint256 _amount, string memory _name, string memory _symbol) ERC20(_name, _symbol) {
